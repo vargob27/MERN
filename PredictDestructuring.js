f4cf2a54513bd0f46277bb62c5c6105b14bb68cf
Bryce <80635028+vargob27@users.noreply.github.com>
2021-09-01 09:24:38 -0400

    Predict Destructuring

---
+++ PredictDestructuring.js
@@ -0,0 +1,61 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ , otherRandomCar ] = cars
+//Predict the output
+// Tesla
+// Mercedes
+console.log(randomCar)
+console.log(otherRandomCar)
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+// will give error because name is not defined and break
+console.log(name);
+console.log(otherName);
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+// 12345
+// undefined
+
+console.log(password);
+console.log(hashedPassword);
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+// false
+// true
+console.log(first == second);
+console.log(first == third);
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+// value
+// [1, 2, 1, 8, 3, 3]
+// 1
+// 5
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
