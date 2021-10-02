a440d64210e1764020720c5abbb08dd764d8673e
Bryce <80635028+vargob27@users.noreply.github.com>
2021-10-02 15:24:10 -0400

    fixed error display and added validations

--- newProject/client/src/components/PersonForm.js
+++ newProject/client/src/components/PersonForm.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const PersonForm = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
+    const [errorMessage, setErrorMessage] = useState({});
     
     const onSubmitHandler = e => {
         e.preventDefault();
@@ -12,16 +13,30 @@ const PersonForm = () => {
             firstName,
             lastName
         })
-            .then(res=>console.log(res))
-            .catch(err=>console.log(err))
+            .then(res=>{
+                if(res.data.errors) {
+                    setErrorMessage(res.data.errors);
+                } else {console.log(res);}
+            })
+            .catch(err=>{
+                console.log(err);
+            })
     };
 
     return (
         <form onSubmit={onSubmitHandler}>
+            {errorMessage.firstName?
+                <p>{errorMessage.firstName.message}</p>
+                :null
+            }
             <p>
                 <label>First Name</label><br/>
                 <input type="text" onChange = {(e)=>setFirstName(e.target.value)}/>
             </p>
+            {errorMessage.lastName?
+                <p>{errorMessage.lastName.message}</p>
+                :null
+            }
             <p>
                 <label>Last Name</label><br/>
                 <input type="text" onChange = {(e)=>setLastName(e.target.value)}/>
--- newProject/server/models/person.model.js
+++ newProject/server/models/person.model.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const PersonSchema = new mongoose.Schema({
-    firstName:  { type: String },
-    lastName: { type: String }
+    firstName:  { 
+        type: String,
+        required: [true, "First name must be entered"],
+        minlength: [2, "First name must be at least 2 characters long"]
+    },
+    lastName: { 
+        type: String,
+        required: [true, "Last name must be entered"],
+        minlength: [2, "Last name must be at least 2 characters long"]
+    }
 }, { timestamps: true });
 module.exports = mongoose.model('Person', PersonSchema);
